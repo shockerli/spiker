9306bf213dbf2f297aec58de7b96aef3b7203aae
shockerli <shockerli@upfor.club>
2020-12-18 19:45:25 +0800

    add TestExecuteWithScope

--- spiker_test.go
+++ spiker_test.go
@@ -1,6 +1,7 @@
 package spiker_test
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/shockerli/spiker"
@@ -22,6 +23,8 @@ func TestEvaluate(t *testing.T) {
 		{`123 in "abc234";`, false},
 		{`23 in "abc234";`, true},
 		{`2 in [1,2,3];`, true},
+		{`!a`, true},
+		{`~8`, -9},
 	}
 
 	for index, tt := range tests {
@@ -67,5 +70,36 @@ b += 2;`},
 		} else if err != nil {
 			t.Error(err.Error())
 		}
+	}
+}
+
+func TestExecuteWithScope(t *testing.T) {
+	var scopes = spiker.NewScopeTable("demo", 1, nil)
+	scopes.Set("a", 3)
+	scopes.Set("b", 4)
+
+	tests := []struct {
+		name    string
+		code    string
+		scope   *spiker.VariableScope
+		wantVal interface{}
+		wantErr bool
+	}{
+		{"nil", "a * b", nil, nil, true},
+		{"mul", "a * b", scopes, float64(12), false},
+		{"add", "a + b", scopes, float64(7), false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotVal, err := spiker.ExecuteWithScope(tt.code, tt.scope)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ExecuteWithScope() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotVal, tt.wantVal) {
+				t.Errorf("ExecuteWithScope() gotVal = %v, want %v", gotVal, tt.wantVal)
+			}
+		})
 	}
 }
